b1fb76f0386fffd3d4ba22bedbe1d0eb322a5903
Fanny Dubosc <fannydubosc@gmail.com>
2018-05-22 19:03:18 +0200

    Change the view controller class by the sub class: WordDetailViewController, to display the appropriate content.

---
+++ Doyouspeakwine/WordDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WordDetailViewController.swift
+//  Doyouspeakwine
+//
+//  Created by Fanny Dubosc on 22/05/2018.
+//  Copyright © 2018 Fanny Dubosc. All rights reserved.
+//
+
+import UIKit
+
+
+
+class WordDetailViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+}
+
