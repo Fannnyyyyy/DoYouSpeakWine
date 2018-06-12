4b1b3e906f5761c4a09ce99f901d83690ff451c4
Fanny Dubosc <fannydubosc@gmail.com>
2018-06-12 19:22:21 +0200

    create the structure called definition

---
+++ Doyouspeakwine/Definition.swift
@@ -0,0 +1,18 @@
+//
+//  Definition.swift
+//  Doyouspeakwine
+//
+//  Created by Fanny Dubosc on 12/06/2018.
+//  Copyright © 2018 Fanny Dubosc. All rights reserved.
+//
+
+import Foundation
+
+
+
+struct Definition {
+	var word : String
+	var definition : String
+	var images : [URL]
+	var relatedWords : [String]
+}
